8889d588238ebb0b7a068270ef3ec3ac26139d71
Maaiz Elahi <maaiz.elahi@postman.com>
2023-05-05 14:36:26 +0530

    Making some changes

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
+var app *newrelic.Application
+var nrErr error
+
 // app, err := newrelic.NewApplication(
 //     newrelic.ConfigAppName("Your Application Name"),
 //     newrelic.ConfigLicense("YOUR_NEW_RELIC_LICENSE_KEY")
@@ -20,27 +23,37 @@ import (
 //     newrelic.ConfigFromEnvironment()
 // )
 
+// const (
+// 	nextCallCtxKey  caddy.CtxKey = "nextCall"
+// )
+
+// // nextCall store the next handler, and the error value return on calling it (if any)
+// type nextCall struct {
+// 	next caddyhttp.Handler
+// 	err  error
+// }
+
 func init() {
 	caddy.RegisterModule(Newrelic{})
 
 	 // Create an Application:
-	 app, err := newrelic.NewApplication(
-        // Name your application
-        // // Fill in your New Relic license key
-        // Add logging:
-		newrelic.ConfigFromEnvironment(),
-		newrelic.ConfigDebugLogger(os.Stdout),
-		newrelic.ConfigAppLogForwardingEnabled(true),
-		newrelic.ConfigCodeLevelMetricsEnabled(true),
-        // Optional: add additional changes to your configuration via a config function:
-        // func(cfg *newrelic.Config) {
-        //     cfg.CustomInsightsEvents.Enabled = false
-        // },
-    )
-    // If an application could not be created then err will reveal why.
-    if err != nil {
-        fmt.Println("unable to create New Relic Application", err)
-    }
+	//  app, err := newrelic.NewApplication(
+    //     // Name your application
+    //     // // Fill in your New Relic license key
+    //     // Add logging:
+	// 	newrelic.ConfigFromEnvironment(),
+	// 	newrelic.ConfigDebugLogger(os.Stdout),
+	// 	newrelic.ConfigAppLogForwardingEnabled(true),
+	// 	newrelic.ConfigCodeLevelMetricsEnabled(true),
+    //     // Optional: add additional changes to your configuration via a config function:
+    //     // func(cfg *newrelic.Config) {
+    //     //     cfg.CustomInsightsEvents.Enabled = false
+    //     // },
+    // )
+    // // If an application could not be created then err will reveal why.
+    // if err != nil {
+    //     fmt.Println("unable to create New Relic Application", err)
+    // }
     // Now use the app to instrument everything!
 }
 
@@ -63,13 +76,75 @@ func (Newrelic) CaddyModule() caddy.ModuleInfo {
 	}
 }
 
-func (m *Newrelic) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+// Provision implements caddy.Provisioner.
+func (nr *Newrelic) Provision(ctx caddy.Context) error {
+	app, nrErr = newrelic.NewApplication(
+		newrelic.ConfigAppName("maaiz-caddy-module-service"),
+        // Fill in your New Relic license key
+        newrelic.ConfigLicense("c6547b52947fb5d2adcd712deccda1339028NRAL"),
+		// newrelic.ConfigFromEnvironment(),
+		newrelic.ConfigDebugLogger(os.Stdout),
+		newrelic.ConfigAppLogForwardingEnabled(true),
+		newrelic.ConfigCodeLevelMetricsEnabled(true),
+		newrelic.ConfigDistributedTracerEnabled(true),
+		newrelic.ConfigCodeLevelMetricsPathPrefix("go-agent/v3"),
+	)
+
+	   // If an application could not be created then err will reveal why.
+	if nrErr != nil {
+        fmt.Println("unable to create New Relic Application", nrErr)
+    }
+	if app != nil {
+        fmt.Println("Maaiz app initialized", app)
+    }
+	// http.HandleFunc(newrelic.WrapHandleFunc(app, "/", dummyHandler))
+
+	http.HandleFunc("/", dummyHandler)
+    // Now use the app to instrument everything!
+	return nrErr
+}
+
+// serveHTTP injects a tracing context and call the next handler.
+// func (nr *Newrelic) serveHTTP(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("Maaiz in small serve http");
+// 	next := r.Context().Value(nextCallCtxKey).(*nextCall)
+// 	next.err = next.next.ServeHTTP(w, r)
+// }
+
+// func (nr *Newrelic) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+// 	fmt.Println("Maaiz in ServeHTTP")
+// 	dummyHandler(w, r)
+// 	// func WrapHandle(app Application, pattern string, handler http.Handler) (string, http.Handler)
+// 	path, handler := newrelic.WrapHandle(app, "/", dummyHandler);
+// 	fmt.Println(path);
+// 	fmt.Println(handler);
+// 	// http.HandleFunc("/", dummyHandler)
+// 	fmt.Println("Maaiz after dummyHandler")
+// 	return next.ServeHTTP(w, r)
+// }
+
+func (nr *Newrelic) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	fmt.Println("Maaiz in ServeHTTP")
+	txn := app.StartTransaction(r.Method + " " + r.Host + r.URL.Path)
+	// txn := app.StartTransaction(r.Method+" "+pattern, txnOptionList...)
+	defer txn.End()
+
+	w = txn.SetWebResponse(w)
+	txn.SetWebRequestHTTP(r)
+
+	r = newrelic.RequestWithTransactionContext(r, txn)
+	
 	return next.ServeHTTP(w, r)
 }
 
+// ServeHTTP implements caddyhttp.MiddlewareHandler.
+// func (ot *Tracing) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+// 	return ot.otel.ServeHTTP(w, r, next)
+// }
+
 // Interface guards
 var (
-	// _ caddy.Provisioner           = (*Newrelic)(nil)
+	_ caddy.Provisioner           = (*Newrelic)(nil)
 	_ caddyhttp.MiddlewareHandler = (*Newrelic)(nil)
 	_ caddy.Module      		  = (*Newrelic)(nil)
 )
